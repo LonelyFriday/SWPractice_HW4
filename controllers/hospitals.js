e089bdb336cb4407aedae7baef4dffbd62d2f928
Napatr <6570073021@student.chula.ac.th>
2023-02-25 16:27:23 +0700

    homework 6

--- controllers/hospitals.js
+++ controllers/hospitals.js
@@ -35,7 +35,7 @@ exports.getHospital= async (req, res, next)=>{
 //@access   Private
 exports.createHospital= async (req, res, next)=>{
     const hospital = await Hospital.create(req.body);
-    res.status(200).json({
+    res.status(201).json({
         success:true,
         data:hospital
     });
