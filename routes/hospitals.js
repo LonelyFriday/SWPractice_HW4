c1d20a23684bf3f2a7089875decac442effa2298
tonkao <napatr.sara@gmail.com>
2023-03-25 22:45:44 +0700

    add VacCenter API

--- routes/hospitals.js
+++ routes/hospitals.js
@@ -14,4 +14,5 @@ router.route('/vacCenters').get(getVacCenters);
 router.route('/').get(getHospitals).post(protect, authorize('admin'), createHospital);
 router.route('/:id').get(getHospital).put(protect, authorize('admin'), updateHospital).delete(protect, authorize('admin'), deleteHospital);
 
-module.exports=router;
+// export
+module.exports=router; 
